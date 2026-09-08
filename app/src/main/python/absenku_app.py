c565dccb2160f7b11a975124c5340ad3ab145f3f
Muh. Irwansyah Irba <ancairba1207-cmd@users.noreply.github.com>
2026-09-08 20:29:37 +0800

    Perbaiki notifikasi setelah simpan tugas harian

--- app/src/main/python/absenku_app.py
+++ app/src/main/python/absenku_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, session, jsonify
+from flask import Flask, request, redirect, url_for, session, jsonify, flash
 import sqlite3
 from datetime import datetime, date, timedelta
 from functools import wraps
