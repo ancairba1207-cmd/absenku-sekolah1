0a571e048e6c4c979bfb7c469c10468e2b751987
Muh. Irwansyah Irba <ancairba1207-cmd@users.noreply.github.com>
2026-09-12 04:49:34 +0800

    Perbaiki ikon status bar Android modern

--- app/src/main/java/com/absenku/sekolah/MainActivity.java
+++ app/src/main/java/com/absenku/sekolah/MainActivity.java
@@ -135,7 +135,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         getWindow().setStatusBarColor(android.graphics.Color.rgb(37, 99, 235));
-        getWindow().getDecorView().setSystemUiVisibility(0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().getInsetsController().setSystemBarsAppearance(
+                    0,
+                    android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            );
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(0);
+        }
 
         if (getIntent() != null && getIntent().hasExtra("notification_route")) {
             notificationRoute = getIntent().getStringExtra("notification_route");
