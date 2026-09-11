e8067c4595513e2ea63568937750818ab6b5b2a7
Muh. Irwansyah Irba <ancairba1207-cmd@users.noreply.github.com>
2026-09-12 00:02:01 +0800

    Tes popup JavaScript setelah scan

--- app/src/main/python/absenku_app.py
+++ app/src/main/python/absenku_app.py
@@ -10102,7 +10102,7 @@ function startScanner(){{
       .then(d=>{{
         if(d.ok) {{
           successFeedback();
-          showSuccessPopup(d.nama,d.jam);
+          alert('POPUP TEST BERHASIL'); showSuccessPopup(d.nama,d.jam);
           if({status!r}==='Pulang') pulangFeedback();
           document.getElementById('scan-status').textContent='✅ Scan berhasil';
         }} else {{
