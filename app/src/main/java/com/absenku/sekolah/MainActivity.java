54e73b748e32135490ca3b138f84e38370f22c22
Muh. Irwansyah Irba <ancairba1207-cmd@users.noreply.github.com>
2026-09-14 10:17:41 +0800

    Perbaiki pemilih dokumen WebView

--- app/src/main/java/com/absenku/sekolah/MainActivity.java
+++ app/src/main/java/com/absenku/sekolah/MainActivity.java
@@ -407,7 +407,6 @@ public class MainActivity extends Activity {
                     // Jangan dipaksa menjadi image/*.
                     Intent intent = fileChooserParams.createIntent();
                     intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
                     intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
 
                     startActivityForResult(
